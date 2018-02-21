0d4dc60e1578829298eeb7a89f35ca8e71c9a6b4
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-02-21 16:19:41 +0800

    Add one more test for Header

--- __tests__/Header.test.js
+++ __tests__/Header.test.js
@@ -8,10 +8,16 @@ import App from '../components/Header.js';
 
 describe('With Enzyme', () => {
   const app = shallow(<App />);
-  it('shows Reddigg header', () => {
-    expect(app.find('h1').text()).toEqual('Reddigg');
-  });
   it('renders one <Jumbotron> element', () => {
     expect(app.find(Jumbotron).length).toBe(1);
   });
+  describe('Inside the <Jumbotron>', () => {
+    const jumbotron = app.find(Jumbotron);
+    it('shows Reddigg header', () => {
+      expect(jumbotron.find('h1').text()).toEqual('Reddigg');
+    });
+    it('provides at least one description', () => {
+      expect(jumbotron.find('p').length).toBeGreaterThan(0);
+    });
+  })
 });

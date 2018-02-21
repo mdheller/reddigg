d16cbc6c3aaee3d22c6b128b4363a43e4ef122c2
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-02-21 16:56:54 +0800

    Add coveralls

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  collectCoverageFrom: [
+    "**/*.js",
+    "!**/node_modules/**",
+    "!**/.next/**",
+    "!**/coverage/**",
+    "!jest.config.js"
+  ],
   setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 };

aed7d3d2e2553ba72750e66e8f7979120bd25c01
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-02-21 18:53:32 +0800

    Exclude server.js from coverage

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
     "!**/node_modules/**",
     "!**/.next/**",
     "!**/coverage/**",
-    "!jest.config.js"
+    "!jest.config.js",
+    "!server.js"
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],

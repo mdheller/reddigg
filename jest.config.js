927de39690c8a9b3b216d7c8038c7701a414273b
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-02-23 12:34:45 +0800

    Refactor according to codacy

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   collectCoverageFrom: [
-    "**/*.js",
-    "!**/node_modules/**",
-    "!**/.next/**",
-    "!**/coverage/**",
-    "!jest.config.js",
-    "!server.js"
+    '**/*.js',
+    '!**/node_modules/**',
+    '!**/.next/**',
+    '!**/coverage/**',
+    '!jest.config.js',
+    '!server.js',
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],

7a863f8844d993f885958a234b85935c441aadcf
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-02-24 19:54:03 +0800

    Add comments

--- backend.js
+++ backend.js
@@ -1,18 +1,20 @@
-const express = require('express');
+// Node Libraries
 const bodyParser = require('body-parser');
-const TopicManager = require('./models/TopicManager');
+const express = require('express');
 
 const app = express();
-
-const tm = new TopicManager();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Import model and initiate
+const TopicManager = require('./models/TopicManager');
+
+const tm = new TopicManager();
+
+// Helper functions
 const throwError = (res, e) => {
   res.status(400).json({ success: false, error: e });
 };
-
 const tryAction = (res, action) => {
   try {
     action();
@@ -22,11 +24,12 @@ const tryAction = (res, action) => {
   }
 };
 
-
+// Fetch all topics
 app.get('/all', (req, res) => {
   res.status(200).json({ success: true, topics: tm.rank });
 });
 
+// Submit new topic
 app.post('/new', (req, res) => {
   // Validate request
   if (req.body.title !== undefined) {
@@ -36,10 +39,12 @@ app.post('/new', (req, res) => {
   }
 });
 
+// Upvote action
 app.put('/upvote/:id', (req, res) => {
   tryAction(res, () => { tm.upvote(req.params.id); });
 });
 
+// Downvote action
 app.put('/downvote/:id', (req, res) => {
   tryAction(res, () => { tm.downvote(req.params.id); });
 });
--- models/TopicManager.js
+++ models/TopicManager.js
@@ -1,5 +1,9 @@
 const Topic = require('./Topic');
 
+// TopicManager uses two arrays:
+// topics and rank to store topics sorted by ID and score respectively.
+// Each array element contains only a pointer to the Topic object, so there is
+// only one single source of truth and no wasted memory space.
 class TopicManager {
   constructor() {
     // Array of topics listed by ID
@@ -9,10 +13,14 @@ class TopicManager {
     // Index where to insert the next new topic
     this.insertion_point = 0;
   }
+  // Check whether the id is invalid
   get(id) {
     if (id < 0 || id > this.topics.length - 1) throw Error('Invalid ID');
     return this.topics[id];
   }
+  // Insert the new topic into the insertion_point
+  // Possible performance improvement: splice might be slower than creating a new
+  // Array and discarding the old Array depending on JavaScript engine.
   add(title) {
     const topic = new Topic(title, this.topics.length);
     this.topics.push(topic);
@@ -27,6 +35,7 @@ class TopicManager {
     if (this.get(id).score === 0) this.insertion_point -= 1;
     this.adjust(id, -1);
   }
+  // Re-sort this.rank in descending order using the built-in Array.prototype.sort function
   adjust(id, amount) {
     const topic = this.get(id);
     topic.score += amount;
